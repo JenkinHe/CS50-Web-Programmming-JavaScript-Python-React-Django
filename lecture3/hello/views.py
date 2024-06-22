4bd7173bd554480dad36cec74ee2f65a98f1074e
Jenkibear <127636124+JenkinHe@users.noreply.github.com>
2024-06-22 15:46:55 +1000

    connecting html files to python through render

--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -3,10 +3,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello,world!")
+    return render(request,"hello/index.html")
 
 def brian(request):
     return HttpResponse("Hello,Brian")
 
 def greet(request,name):
-    return HttpResponse(f"Hello,{name}")
+    return render(request,"hello/greet.html",{
+        "name":name.capitalize()
+    })
